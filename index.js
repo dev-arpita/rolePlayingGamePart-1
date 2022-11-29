326113f15b44bbad7b57f84779836d49c428e339
Arpita Roy <arpita.roy.workar@gmail.com>
2022-11-29 19:09:10 +0530

    Object destructuring in renderCharecter()

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-/*
-CHALLENGE
-1. Convert our consts into two objects called
-"monster" and "hero".
-2. Update the renderCharacter() function so that it accepts
-a single object "data" as its parameter instead of five string/numbers,
-reducing the number of arguments to pass in from five to one.
-3. Update the template now each variable is coming from "data".
-4. Update the function call.
-*/
+
+// CHALLENGE
+// 1. Deconstruct the data object
+// 2. Update the template string as necessary
 
 const hero = {
     elementId: "hero",
@@ -26,13 +20,14 @@ const monster = {
 }
 
 function renderCharacter(data) {
+    const {elementId, name, avatar, health, diceRoll} = data
     document.getElementById(data.elementId).innerHTML =
         `<div class="character-card">
-            <h4 class="name"> ${data.name} </h4>
-            <img class="avatar" src="${data.avatar}" />
-            <div class="health">health: <b> ${data.health} </b></div>
+            <h4 class="name"> ${name} </h4>
+            <img class="avatar" src="${avatar}" />
+            <div class="health">health: <b> ${health} </b></div>
             <div class="dice-container">
-                <div class="dice"> ${data.diceRoll} </div>
+                <div class="dice"> ${diceRoll} </div>
             </div>
         </div>`
 }
