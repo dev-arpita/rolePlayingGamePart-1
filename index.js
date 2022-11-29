84fcb715f164e91aaaa80be033ae8a0cac515cff
Arpita Roy <arpita.roy.workar@gmail.com>
2022-11-29 18:31:18 +0530

    Modify renderCharacter()

--- index.js
+++ index.js
@@ -1,37 +1,41 @@
 /*
 CHALLENGE
-1. Strip out the hero and monster data (element id, name, avatar,
-health and dice score) and store them in variables
-
-2. Write a renderCharacter() function that accepts the 5 new variables
-as paramaters and renders out a character with this data
-
-3. Call renderCharacter() twice. Once with the hero variables and
-once with the monster variables to that both are rendered
+1. Convert our consts into two objects called
+"monster" and "hero".
+2. Update the renderCharacter() function so that it accepts
+a single object "data" as its parameter instead of five string/numbers,
+reducing the number of arguments to pass in from five to one.
+3. Update the template now each variable is coming from "data".
+4. Update the function call.
 */
 
-const heroElementId = "hero"
-const heroName = "Wizard"
-const heroAvatar = "images/wizard.png"
-const heroHealth = 60
-const heroDiceRoll = 6
-
-const monsterElementId = "monster"
-const monsterName = "Orc"
-const monsterAvatar = "images/orc.png"
-const monsterHealth = 10
-const monsterDiceRoll = 4
-
-function renderCharacter(elementId, name, avatar, health, diceScore) {
-    document.getElementById(elementId).innerHTML = `
-    <div class="character-card">
-        <h4 class="name"> ${name} </h4>
-        <img class="avatar" src="${avatar}"/>
-        <p class="health">health: <b> ${health} </b></p>
-        <div class="dice-container"><div class="dice"> ${diceScore} </div></div>
-    </div>
-`
+const hero = {
+    elementId: "hero",
+    name: "Wizard",
+    avatar: "images/wizard.png",
+    health: 60,
+    diceRoll: 6
 }
 
-renderCharacter(heroElementId, heroName, heroAvatar, heroHealth, heroDiceRoll)
-renderCharacter(monsterElementId, monsterName, monsterAvatar, monsterHealth, monsterDiceRoll)
+const monster = {
+    elementId: "monster",
+    name: "Orc",
+    avatar: "images/orc.png",
+    health: 10,
+    diceRoll: 4
+}
+
+function renderCharacter(data) {
+    document.getElementById(data.elementId).innerHTML =
+        `<div class="character-card">
+            <h4 class="name"> ${data.name} </h4>
+            <img class="avatar" src="${data.avatar}" />
+            <div class="health">health: <b> ${data.health} </b></div>
+            <div class="dice-container">
+                <div class="dice"> ${data.diceRoll} </div>
+            </div>
+        </div>`
+}
+
+renderCharacter(hero);
+renderCharacter(monster);
