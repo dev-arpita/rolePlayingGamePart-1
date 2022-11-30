f517e8305fb0caf7f7fe394fcea6d6f7a0653c84
Arpita Roy <arpita.roy.workar@gmail.com>
2022-11-30 08:05:44 +0530

    The problem with for loops

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const hero = {
     name: "Wizard",
     avatar: "images/wizard.png",
     health: 60,
-    diceRoll: 3,
+    diceRoll: [3, 1, 4],
     diceCount: 3
 }
 
@@ -12,40 +12,34 @@ const monster = {
     name: "Orc",
     avatar: "images/orc.png",
     health: 10,
-    diceRoll: 4,
+    diceRoll: [2],
     diceCount: 1
 }
 
 function renderCharacter(data) {
     const {elementId, name, avatar, health, diceRoll, diceCount } = data;
+    let diceHtml = '';
 
-/*
-CHALLENGE
-1. declare a let called diceHtml and initialize it with an empty
-string.
-2. Use a for loop to update diceHtml so that it contains the
-HTML for our dice. The number of dice needed is specificed in
-the diceCount property of the objects.
-3. Each dice should have the following HTML: <div class="dice">6</div>
-4. For now, each dice will display 6
-5. Swap out the diceRoll variable for diceHtml in the template
-*/
+// CHALLENGE
+// Update this for loop so it uses a value from the
+// new diceRoll array to render out the dice so the
+// wizard's dice have values of 3, 1 and 4, and the
+// orc's single dice has a value of 2.
 
-let diceHtml = ''
 
-for (let i = 0; i<diceCount; i++){
-    diceHtml += `<div class="dice">6</div>`
+for (let i = 0; i < diceRoll.length; i++) {
+    diceHtml += `<div class="dice">${diceRoll[i]}</div>`
 }
 
     document.getElementById(elementId).innerHTML =
-        `<div class="character-card">
-            <h4 class="name"> ${name} </h4>
-            <img class="avatar" src="${avatar}" />
-            <div class="health">health: <b> ${health} </b></div>
-            <div class="dice-container">
-                ${diceHtml}
-            </div>
-        </div>`
+    `<div class="character-card">
+        <h4 class="name"> ${name} </h4>
+        <img class="avatar" src="${avatar}" />
+        <div class="health">health: <b>${health}</b></div>
+        <div class="dice-container">
+            ${diceHtml}
+        </div>
+    </div>`;
 }
 
 renderCharacter(hero);
